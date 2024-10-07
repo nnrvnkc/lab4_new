5d67e9cceb1bc2c0e03c7f2d2f6a37f972e9de3c
nnrvnkc <svitlana.heriak.kn.2021@lpnu.ua>
2024-10-07 17:35:05 +0300

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Створюємо віртуальне середовище, якщо його немає
+python -m venv venv
+
+REM Активуємо віртуальне середовище
+call venv\Scripts\activate
+
+REM Встановлюємо залежності
+python -m pip install -r requirements.txt
+
+REM Запускаємо тести
+python -m unittest discover -s tests
